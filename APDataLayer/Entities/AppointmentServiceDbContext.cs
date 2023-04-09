47bba2334fb38ff59b39462ef7be21693c99e9ea
SrinuSamarothu <srinu143.h@gmail.com>
2023-04-10 00:17:42 +0530

    connection string updated

--- APDataLayer/Entities/AppointmentServiceDbContext.cs
+++ APDataLayer/Entities/AppointmentServiceDbContext.cs
@@ -19,7 +19,7 @@ public partial class AppointmentServiceDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=tcp:teamperseverance.database.windows.net,1433;Initial Catalog=appointmentsdb;Persist Security Info=False;User ID=manoj;Password=Team@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+        => optionsBuilder.UseSqlServer("Server=tcp:teamperseverance.database.windows.net,1433;Initial Catalog=appointmentsdb;Persist Security Info=False;User ID=manoj;Password=Team@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
